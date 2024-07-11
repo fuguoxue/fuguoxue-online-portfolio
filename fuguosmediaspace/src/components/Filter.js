96f33de0b313bfa931750944b8857a2ddbf8e63a
fuguoxue-nyu <144282221+fuguoxue-nyu@users.noreply.github.com>
2024-07-11 15:03:20 +0800

    Added filter effects

--- fuguosmediaspace/src/components/Filter.js
+++ fuguosmediaspace/src/components/Filter.js
@@ -3,6 +3,7 @@
 import { useState, useEffect } from "react";
 import projectList from "../projectData.json";
 import WorkGallery from "./WorkGallery";
+import { AnimatePresence } from "framer-motion";
 
 function Capitalize(str) {
   return str.charAt(0).toUpperCase() + str.slice(1);
@@ -10,12 +11,13 @@ function Capitalize(str) {
 
 function Filter() {
   // Define category priorities
-  const categoryPriority = ["All",
+  const categoryPriority = [
+    "All",
     "Interactive Installation",
     "Creative Coding",
     "Game",
     "Animation",
-    "UX Design"
+    "UX Design",
   ];
 
   const [all, setAll] = useState([]);
@@ -31,20 +33,24 @@ function Filter() {
     if (activeCat === "All") {
       setFiltered(all);
     } else {
-      setFiltered(all.filter(project => Capitalize(project.category) === activeCat));
+      setFiltered(
+        all.filter((project) => Capitalize(project.category) === activeCat)
+      );
     }
   }, [activeCat, all]);
 
   const extractAndSortDistinctValues = (data) => {
-    const categories = data.map((item) => Capitalize(item.category)); 
+    const categories = data.map((item) => Capitalize(item.category));
     const distinctCategories = ["All", ...new Set(categories)];
 
     // Sort the distinct values based on the defined priorities
     const sortedValues = distinctCategories.sort((a, b) => {
       if (a === "All") return -1;
       if (b === "All") return 1;
-      return (categoryPriority.indexOf(a) || Infinity) - (categoryPriority.indexOf(b) || Infinity);
-      
+      return (
+        (categoryPriority.indexOf(a) || Infinity) -
+        (categoryPriority.indexOf(b) || Infinity)
+      );
     });
 
     return sortedValues;
@@ -54,22 +60,34 @@ function Filter() {
 
   const categoryList = distinctCategories.map((category) => (
     <li className="me-4 md:me-6 text-gray-900">
-      <button className="capitalize hover:underline" onClick={()=>setActiveCat(category)} key={categoryPriority.indexOf(category)}>{category}</button>
+      <button
+        className={
+          activeCat === category
+            ? "underline capitalize font-medium"
+            : "capitalize hover:underline"
+        }
+        onClick={() => setActiveCat(category)}
+        key={categoryPriority.indexOf(category)}
+      >
+        {category}
+      </button>
     </li>
   ));
 
   return (
     <div className="py-5">
-      <ul className="flex flex-wrap items-center justify-center text-gray-900 dark:text-white">
+      <ul className="flex flex-wrap items-center justify-center text-gray-900 dark:text-white mb-4">
         {categoryList}
       </ul>
       <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {filtered.map(project => (
-          <WorkGallery key={project.id} project={project} />
-        ))}
+        <AnimatePresence>
+          {filtered.map((project) => (
+            <WorkGallery key={project.id} project={project} />
+          ))}
+        </AnimatePresence>
       </div>
-    </div>    
+    </div>
   );
-};
+}
 
 export default Filter;
--- fuguosmediaspace/src/components/WorkGallery.js
+++ fuguosmediaspace/src/components/WorkGallery.js
@@ -1,16 +1,25 @@
+import { motion } from "framer-motion";
+
 const WorkGallery = ({ project }) => {
   return (
-    <div>
+    <motion.div
+      layout
+      animate={{ opacity: 1 }}
+      initial={{ opacity: 0 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.2 }}
+    >
       <div className="pb-2 w-full h-60 overflow-hidden min-w-80">
-      <img
-        src={project.imgURL}
-        className="object-cover size-full"
-        alt={project.name}
-      ></img></div>
+        <img
+          src={project.imgURL}
+          className="object-cover size-full"
+          alt={project.name}
+        ></img>
+      </div>
       <h3 className="text-xl font-semibold font-mono">{project.name}</h3>
       <p>{project.category}</p>
       <p>{project.year}</p>
-    </div>
+    </motion.div>
   );
 };
 
