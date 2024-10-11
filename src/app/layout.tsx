c6d1259bd3f6487416d02386e5cfa4cdbda0f881
fuguoxue-nyu <144282221+fuguoxue-nyu@users.noreply.github.com>
2024-10-11 14:01:28 -0400

    Added SpeedInsights to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inconsolata } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 import localFont from "next/font/local";
 import "./globals.css";
@@ -57,6 +58,7 @@ export default function RootLayout({
       >
         {children} {/* Content will be rendered here */}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
